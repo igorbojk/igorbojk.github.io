3067cacffc08fff3b6c0cb6d498179febbeb1cea
Igor <igorbojk@gmail.com>
2020-10-05 14:31:33 +0300

    pwa 2

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'static-cache-v0';
+const staticCacheName = 'static-cache-v2';
+const dynamicCacheName = 'dynamic-cache-v0';
 
 const staticAssets = [
     './',
@@ -7,7 +8,8 @@ const staticAssets = [
     './images/icons/icon-96x96.png',
     './styles/style.css',
     './js/app.js',
-    './js/index.js'
+    './js/index.js',
+    './offline.html'
 ];
 
 self.addEventListener('install', async event => {
@@ -17,15 +19,40 @@ self.addEventListener('install', async event => {
 });
 
 self.addEventListener('activate', async event => {
+    const cachesKeys = await caches.keys();
+    const checkKeys = cachesKeys.map(async key => {
+        if (staticCacheName !== key) {
+            await caches.delete(key);
+        }
+    });
+    await Promise.all(checkKeys);
     console.log('Service worker has been activated');
 });
 
 self.addEventListener('fetch', event => {
     console.log(`Trying to fetch ${event.request.url}`);
-    event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                return cachedResponse || fetch(event.request);
-            })
-    );
-})
+    event.respondWith(checkCache(event.request));
+});
+
+async function checkCache(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || checkOnline(req);
+};
+
+async function checkOnline(req) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const res = await fetch(req);
+        await cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        const cachedRes = await cache.match(req);
+        if (cachedRes) {
+            return cachedRes;
+        } else if (req.url.indexOf('.html' !== -1)) {
+            return caches.match('./offline.html');
+        } else {
+            return caches.match('./images/no-image.png');
+        }
+    }
+}
